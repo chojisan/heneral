92d21738d62d1338ce939a122c0020bc05c65774
Choji Takada <choji.takada@gmail.com>
2018-09-29 20:56:47 +0800

    Create heneral.php

---
+++ config/heneral.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+    /*
+     Administration panel
+     */
+    'admin-prefix' => 'backend',
+
+
+    /**
+     * TEMPLATE SECTION
+     */
+
+    /*
+	 Location of template
+    */
+    'template_path' => base_path() . '/templates',
+
+    /**
+     * Template assets
+     */
+    'template_asset' => '',
+
+    /*
+	 Active template
+    */
+    'template_admin_default' => 'default',
+];
